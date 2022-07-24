b393f463c1210b1c9060e40806c6a7e2597aa84f
jkpark <inmosty@gmail.com>
2022-07-24 15:54:31 +0900

    COMMIT

---
+++ src/main/java/hello/jdbcstudy/service/MemberServiceV2.java
@@ -0,0 +1,61 @@
+package hello.jdbcstudy.service;
+
+import hello.jdbcstudy.domain.Member;
+import hello.jdbcstudy.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+    /**
+     * 트랜잭션 - 파라미터연동, 풀을 고려한 종료
+     * */
+    public void accountTransfer(String fromId, String toId,int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+        try {
+            con.setAutoCommit(false);
+            //비지니스 로직
+            bizLogic(fromId, toId, money, con);
+            con.commit();//성공시 커밋
+        }catch (Exception e){
+            con.rollback();
+            throw new IllegalStateException(e);
+        }finally {
+            release(con);
+        }
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money, Connection con) throws SQLException {
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if(con != null){
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            }catch (Exception e){
+                log.error("error",e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
